104c3b48fef737a57a486af603a9ea084fa05c08
Udhaya20001 <udhayaa20001@gmail.com>
2025-09-01 21:36:51 +0530

    Update settings.py

--- timetable_project/settings.py
+++ timetable_project/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'timetable_project',
         'USER' : 'root',
-        'PASSWORD' : 'naveen9840',
+        'PASSWORD' : '1234',
         'HOST' : 'localhost',
         'PORT' : '3306',
     }
